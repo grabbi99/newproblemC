0ef44e103ad01e104d6aa71eb6e7c55e93b24459
grabbi99 <mdrabbi.hossain9@gmail.com>
2017-09-30 03:56:19 +0600

    First solv add

---
+++ all-problem.c
@@ -0,0 +1,20 @@
+#1 . Write a C program to enter length and breadth of a rectangle and find its perimeter.
+
+#include<stdio.h>
+
+int main ()
+{
+    float length, breadth, perimeter;
+    printf("Enter length of Rectangle \n");
+    scanf("%f", &length);
+    printf("Enter Breadth of Rectangle \n");
+    scanf("%f", &breadth);
+
+    // Perimeter of Rectangle = 2 X(Length + Width)
+    perimeter = 2* (length + breadth);
+
+    printf("Perimeter of Rectangle: %f \n", perimeter);
+
+    return 0;
+
+}
